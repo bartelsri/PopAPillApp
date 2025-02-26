0431ea1b0d51994708e9eb33cfb36c9aa391b266
jossyris <delpilargj@vcu.edu>
2025-02-26 14:50:24 -0500

    Initial Commit

---
+++ PopAPill/PopAPillApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopAPillApp.swift
+//  PopAPill
+//
+//  Created by jossyris on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct PopAPillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PopAPillTests/PopAPillTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopAPillTests.swift
+//  PopAPillTests
+//
+//  Created by jossyris on 2/26/25.
+//
+
+import Testing
+@testable import PopAPill
+
+struct PopAPillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
