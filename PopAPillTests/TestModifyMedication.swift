ffe2f05500d733d6c3d5f625aa63de5a114bc42b
bartelsri <bartelsrehme@gmail.com>
2025-03-25 13:51:55 -0400

    TestModifyMedication class

--- PopAPillTests/TestModifyMedication.swift
+++ PopAPillTests/TestModifyMedication.swift
@@ -10,4 +10,39 @@ import XCTest
 
 class TestModifyMedication : XCTestCase {
 
+    var medication : ModifyMedication!
+
+    override fun setUp(){
+        super.setUp()
+        medication = ModifyMedication()
+    }
+
+    override fun tearDown(){
+        medication = nil
+        super.tearDown()
+    }
+
+    // test case: modify medication with valid data
+    func testModifyMedicationWithValidData(){
+        medication.name = "Test Medication"
+        medication.dosage = "1"
+        medication.unit = "Pill"
+        medication.frequency = "2"
+
+        let isValid = medication.validate()
+        XCTAssertTrue(isValid)
+        XCTAssertEqual(medication.errorM, "")
+    }
+
+    // test case: modify medication with invalid dosage
+    func testModifyMedicationWithInvalidData(){
+
+    }
+    // test case: modify medication with invalid frequency
+    // test case: modify medication with empty data
+    // test case: add new medication with valid data
+    // test case: add new medication with invalid dosage
+    // test case: add new medication with invalid frequency
+    // test case: add new medication with empty data
+
 }
