528c1013a94d571940eae345e0bc83632bd3eb47
bartelsri <bartelsrehme@gmail.com>
2025-03-26 23:33:11 -0400

    accidentally deleted part of a line, fixed it

---
+++ PopAPill/Views/ModifyMedView.swift
@@ -0,0 +1,21 @@
+//
+// ModifyMedView.swift
+// PopAPill
+//
+// created by bartelsri 3/26/25
+//
+
+import SwiftUI
+
+struct ModifyMedView: View{
+
+    var body : some View {
+
+        VStack{
+
+
+        }
+
+    }
+
+}
