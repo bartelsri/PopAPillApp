845251db5e74124bf1df3fd3aec1d045bff821c1
bartelsri <bartelsrehme@gmail.com>
2025-03-26 20:38:17 -0400

    TestModifyMedication class

---
+++ PopAPill/Models/Medication.swift
@@ -0,0 +1,15 @@
+//
+// Medication.swift
+// PopAPill
+//
+// created by bartelsri 3/26/25
+
+import Foundation
+
+struct Medication: Codable {
+   let medName: String
+   let dosage: Int
+   let unit: String
+   let frequency: Int
+
+}
--- PopAPill/ViewModels/ModifyMedicationViewModel.swift
+++ PopAPill/ViewModels/ModifyMedicationViewModel.swift
@@ -3,6 +3,9 @@
 // PopAPill
 //
 // created by bartelsri 3/26/25
+//
+
+import Foundation
 
 class ModifyMedicationViewModel: ObservableObject{
     @Published var medName = ""
@@ -12,7 +15,6 @@ class ModifyMedicationViewModel: ObservableObject{
 
     @Published var errorM = ""         //error message
     @Published var showError = false   //flag to show error message
-
 
 
 
