bae259b8af0df987107c8be9e6ceffa9988b3be0
jossyris <delpilargj@vcu.edu>
2025-04-13 19:19:07 -0400

    Updates in main for user notifications
    Updates in main for user notifications, permissions and presentation.

--- PopAPill/Other/PopAPillApp.swift
+++ PopAPill/Other/PopAPillApp.swift
@@ -6,9 +6,11 @@
 //
 import FirebaseCore
 import SwiftUI
+import UserNotifications
 
 @main
 struct PopAPillApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     
     init() {
         FirebaseApp.configure()
@@ -19,5 +21,46 @@ struct PopAPillApp: App {
         WindowGroup {
             MainView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    //when app is finished launching
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Request notification permission from the user
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {granted, error in
+            //Check if there were any errors while requesting
+            if let error = error {
+                print("Error requestion notification permission: \(error)")
+           //Either granted or denied
+            } else if granted {
+                print("Notifications granted")
+            } else {
+                print("Notifications not granted")
+            }
+        }
+        
+        //Observer to reset the badge when the app is active
+        NotificationCenter.default.addObserver(self, selector: #selector(resetBadge), name: UIApplication.didBecomeActiveNotification, object: nil)
+        
+        //Set the delegate to self so we can manage it
+        UNUserNotificationCenter.current().delegate = self
+        
+        return true
+
+    }
+    
+    //Allows notifications to be shown, even in foreground and within app
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping ( UNNotificationPresentationOptions) -> Void ) {
+        
+        // Shows the notification with a banner, sound, and badge
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+    // Resets the app icon badge number to 0
+    @objc func resetBadge() {
+        UNUserNotificationCenter.current().setBadgeCount(0)
     }
 }
