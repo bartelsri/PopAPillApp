59a164f2e10ea9311f8956507494c56b0ed4c33c
jossyris <delpilargj@vcu.edu>
2025-04-15 16:41:52 -0400

    Update MainViewModel.swift
    Added comments in main

--- PopAPill/ViewModels/MainViewModel.swift
+++ PopAPill/ViewModels/MainViewModel.swift
@@ -8,21 +8,25 @@
 import Foundation
 import FirebaseAuth
 
+//ViewModel to manage Firebase Authentication state
 class MainViewModel: ObservableObject {
+    // Keeps track of the current user ID
     @Published var currID: String = ""
+    //Firebase Auth state listener handler
     private var handler: AuthStateDidChangeListenerHandle?
     
     init() {
+        // Adds listener to observe authentication state changes
         self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
             DispatchQueue.main.async {
-                
-                
+                // Updates ID when user is logs in or out
                 self?.currID = user?.uid ?? ""
             }
             
         }
     }
     
+    // Checks if user is currently signed in 
     public var isSignedIn: Bool {
         return Auth.auth().currentUser != nil
         
