33ccaf19e0f8e03cf4f24b618204e29c9997ce91
bartelsri <bartelsrehme@gmail.com>
2025-04-27 21:19:02 -0400

    updates to patient profile view  / model

--- PopAPill/Views/PatientProfileView.swift
+++ PopAPill/Views/PatientProfileView.swift
@@ -18,78 +18,86 @@ struct PatientProfileView: View {
 
     
     var body: some View {
-        VStack {
-            //header
-            RoundedRectangle(cornerRadius: 0)
-                .foregroundColor(Color(red: 1.0, green: 0.81, blue: 0.86))
-                .shadow(color: Color.black.opacity(0.17), radius: 10,y: 5)
-            
-                .frame(width: UIScreen.main.bounds.width * 3, height: 300)
-            
-                .offset(y:-280)
-            Text("Patient Profile")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-            //.padding(30)
-                .offset(y:-400)
-            
-            //Display patient info
-            /*Text("Name: \(patient.name)")
-             .font(.title2)
-             .bold()
-             //.padding()
-             .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-             .offset(x:-90, y:-275)
-             
-             Text("Contact: \(patient.email)")
-             .font(.body)
-             //.padding()
-             .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-             .offset(x: -60, y:-250)*/
-        } //end of first vstack
-        VStack(alignment: .leading, spacing: 10) {
-            Text("Name: \(patient.name)")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-            
-            Text("Contact: \(patient.email)")
-                .font(.title2)
-                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-        } //end of patient vstack
-        .offset(x:-40, y:-285)
-        
-        //.padding()
-        
-        //medication list section
-        VStack(alignment: .leading, spacing: 10){
-            Text("Medications:")
-                .font(.title2)
-                .bold()
-                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-                .offset(x: -100, y:-225)
-            
-            if viewModel.medications.isEmpty {
-                Text("No medications listed.")
-                    .foregroundColor(.gray)
-            } else {
-                Text("Medications:")
-                    .font(.headline)
-                    .padding(20)
-                
-                ForEach(viewModel.medications) {medication in
-                    Text("\(medication.name) - \(medication.dosage) \(medication.unit) \(medication.frequency) times per day")
-                }
-            }
-        } //end of body
-    } //end of struct
-    
-    
-    /*//Display medication list
-     Text("Medication(s):")
-     ForEach(patient.medications) { medication in
-     Text("\(medication.name) - \(medication.dosage) \(medication.dosageUnit) \(medication.frequency) times per day")
-     }*/
-}
+        ScrollView {
+            VStack {
+                //header
+                VStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(Color(red: 1.0, green: 0.81, blue: 0.86))
+                        .shadow(color: Color.black.opacity(0.17), radius: 10,y: 5)
+
+                        .frame(width: UIScreen.main.bounds.width * 3, height: 300)
+
+                        .offset(y:-280)
+
+                    Text("Patient Profile")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                        .offset(y:-400)
+                } // end head VStack
+                //.padding()
+
+                //patient info
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Name: \(patient.name)")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+
+                    Text("Contact: \(patient.email)")
+                        .font(.title2)
+                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                } //end of patient vstack
+                .offset(x:-40, y:-285)
+
+                //.padding(.horizontal)
+
+                //medication list section
+                VStack(alignment: .leading, spacing: 10){
+                    Text("Medications:")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                        .offset(x: -100, y:-225)
+
+                    //if no meds listed
+                    if viewModel.medications.isEmpty {
+                        Text("No medications listed.")
+                            .foregroundColor(.gray)
+                    } else {   //Display medication list
+                        Text("Medications:")
+                            .font(.headline)
+                            .padding(20)
+
+                        //for each med object in view model for the patinet
+                        ForEach(viewModel.medications) { medication in
+                            HStack{
+                                Text("\(medication.name) - \(medication.dosage) \(medication.dosageUnit) \(medication.frequency) times per day")
+                                    .font(.body)
+                                    .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+
+                                    // move button down
+                                    Spacer()
+
+                                    // button to navigate to modify meds
+                                    NavigationLink(destination: ModifyMedView(medication: medication, patientId: patientId)){
+                                        Text("Modify")
+                                            .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                                    }
+                            } //end HStack
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+
+                        } //end ForEach (meds)
+
+                    } //end else
+                } // end VStack (medication list)
+                .padding(.horizontal)
+            } // end main Vstack
+            .padding(.top)
+        } //end scroll view
+    } //end of body
+} //end of struct
 
