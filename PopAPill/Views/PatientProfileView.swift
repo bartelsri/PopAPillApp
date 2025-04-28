7985c3565b14c38fb58e7f19baf84c3ceabd267a
bartelsri <bartelsrehme@gmail.com>
2025-04-28 00:10:31 -0400

    breaking up the stacks into structs for compiling

--- PopAPill/Views/PatientProfileView.swift
+++ PopAPill/Views/PatientProfileView.swift
@@ -23,88 +23,104 @@ struct PatientProfileView: View {
         self._viewModel = StateObject(wrappedValue: PatientProfileViewModel(patientId: patientId))
     }
 
-    
-    var body: some View {
-        ScrollView {
-            VStack {
-                //header
-                VStack{
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundColor(Color(red: 1.0, green: 0.81, blue: 0.86))
-                        .shadow(color: Color.black.opacity(0.17), radius: 10,y: 5)
-
-                        .frame(width: UIScreen.main.bounds.width * 3, height: 300)
-
-                        .offset(y:-280)
-
-                    Text("Patient Profile")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-                        .offset(y:-400)
-                } // end head VStack
-                //.padding()
-
-                //patient info
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Name: \(patient.name)")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-
-                    Text("Contact: \(patient.email)")
-                        .font(.title2)
-                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-                } //end of patient vstack
-                .offset(x:-40, y:-285)
-
-                //.padding(.horizontal)
-
-                //medication list section
-                VStack(alignment: .leading, spacing: 10){
-                    Text("Medications:")
-                        .font(.title2)
-                        .bold()
-                        .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-                        .offset(x: -100, y:-225)
-
-                    //if no meds listed
-                    if viewModel.medications.isEmpty {
-                        Text("No medications listed.")
-                            .foregroundColor(.gray)
-                    } else {   //Display medication list
-                        Text("Medications:")
-                            .font(.headline)
-                            .padding(20)
-
-                        //for each med object in view model for the patinet
-                        ForEach(viewModel.medications) { medication in
-                            HStack{
-                                Text("\(medication.name) - \(medication.dosage) \(medication.dosageUnit) \(medication.frequency) times per day")
-                                    .font(.body)
-                                    .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-
-                                    // move button down
-                                    Spacer()
-
-                                    // button to navigate to modify meds
-                                    NavigationLink(destination: ModifyMedView(medication: medication, patientId: patientId)){
-                                        Text("Modify")
-                                            .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
-                                    }
-                            } //end HStack
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(10)
-
-                        } //end ForEach (meds)
-
-                    } //end else
-                } // end VStack (medication list)
-                .padding(.horizontal)
-            } // end main Vstack
+    var body: some View{
+        Scroll View{
+            VStack{
+                HeaderView()
+                PatientInfoView(patient: patient)
+                MedicationListView(viewModel: viewModel)
+            }
             .padding(.top)
-        } //end scroll view
-    } //end of body
+        }
+    }
+}
+
+//header struct
+struct HeaderView: View{
+    var body: some View {
+            VStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(Color(red: 1.0, green: 0.81, blue: 0.86))
+                    .shadow(color: Color.black.opacity(0.17), radius: 10,y: 5)
+
+                    .frame(width: UIScreen.main.bounds.width * 3, height: 300)
+
+                    .offset(y:-280)
+
+                Text("Patient Profile")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                    .offset(y:-400)
+        }
+    }
+}
+
+//patient info
+struct PatientInfoView: View {
+    var patient: Patient
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Name: \(patient.name)")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+
+            Text("Contact: \(patient.email)")
+                .font(.title2)
+                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+        } //end of patient vstack
+        .offset(x:-40, y:-285)
+    }
+}
+
+//medication list section
+struct MedicationListView: View{
+    @ObservedObject var viewModel: PatientProfileViewModel
+
+    var body: some View{
+        VStack(alignment: .leading, spacing: 10){
+            Text("Medications:")
+                .font(.title2)
+                .bold()
+                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                .offset(x: -100, y:-225)
+
+            //if no meds listed
+            if viewModel.medications.isEmpty {
+                Text("No medications listed.")
+                    .foregroundColor(.gray)
+            } else {   //Display medication list
+                Text("Medications:")
+                    .font(.headline)
+                    .padding(20)
+
+                //for each med object in view model for the patinet
+                ForEach(viewModel.medications) { medication in
+                    HStack{
+                        Text("\(medication.name) - \(medication.dosage) \(medication.dosageUnit) \(medication.frequency) times per day")
+                            .font(.body)
+                            .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+
+                            // move button down
+                            Spacer()
+
+                            // button to navigate to modify meds
+                            NavigationLink(destination: ModifyMedView(medication: medication, patientId: patientId)){
+                                Text("Modify")
+                                    .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.6))
+                            }
+                    } //end HStack
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+
+                } //end ForEach (meds)
+
+            } //end else
+        } // end VStack (medication list)
+        .padding(.horizontal)
+    } //end of body for med
 } //end of struct
 
